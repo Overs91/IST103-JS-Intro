e87b1f013034f6c5cf6b106593a9afecc7aefc44
Overs <abogadoberruecos@gmail.com>
2024-04-10 22:00:23 -0500

    Activity 2, console log

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ alert("Hello World"); //to set an alert
 
 let randompersonName = "Jorge Berruecos";
 let age = 33;
-let isStudent = false;
+let Student = false;
 let address = undefined;
 let favouriteColor = null;
 const birthYear = 1991;
+
+console.log("Person's name is " + randompersonName);
+console.log("Person's age is" + age);
+console.log("Is a student?" + Student);
+console.log("Favourite color is" + favouriteColor);
+console.log("birthyear?" + birthYear);
