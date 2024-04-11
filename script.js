a2db21db2480892f6d720aa8651e94f39c523daa
Overs <abogadoberruecos@gmail.com>
2024-04-10 22:14:21 -0500

    Console log added address

--- script.js
+++ script.js
@@ -10,5 +10,6 @@ const birthYear = 1991;
 console.log("My name is " + Name);
 console.log("I am " + age);
 console.log("Is he studying business? " + Student);
+console.log("My address is " + address);
 console.log("My favourite color is " + favouriteColor);
 console.log("I was born in " + birthYear);
