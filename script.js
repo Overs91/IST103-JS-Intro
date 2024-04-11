8c9caae48d580c5efe3030f5f719b7897836a55e
Overs <abogadoberruecos@gmail.com>
2024-04-10 22:54:47 -0500

    Activity 3, Class 10

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-alert("Hello World"); //to set an alert
+// alert("Hello World");
+
+// Activity 2
 
 let Name = "Jorge Berruecos";
-let age = "33";
+let age = "33 y/o";
 let Student = false;
 let address = undefined;
 let favouriteColor = null;
 const birthYear = 1991;
 
-console.log("My name is " + Name);
+console.log("My name is: " + Name);
 console.log("I am " + age);
-console.log("Is he studying business? " + Student);
-console.log("My address is " + address);
-console.log("My favourite color is " + favouriteColor);
-console.log("I was born in " + birthYear);
+console.log("Is he studying business?: " + Student);
+console.log("My address is: " + address);
+console.log("My favourite color is: " + favouriteColor);
+console.log("I was born in: " + birthYear);
+
+// Activity 3
+
+let whattodo = [
+  "prepare and have my dinner",
+  "finish the lab",
+  "take a shower",
+  "go to sleep",
+];
+
+let myComputer = {
+  board: "Gigabyte",
+  ram: "64 gb ddr5",
+  graphics: "Nvidia Geforce Rtx 4080 ti",
+  cpu: "Core i7 13th gen",
+  inUse: true,
+  monitors: 2,
+};
+
+console.log("What I am doing after class is: " + whattodo[0]);
+console.log("my computer graphics card is: " + myComputer.graphics);
