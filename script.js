0ea83e85b9658a3b2131ea2a85e9bc52eca3d82c
Overs <abogadoberruecos@gmail.com>
2024-04-10 21:54:44 -0500

    Activity 2 p1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Hello World");
+alert("Hello World"); //to set an alert
+
+let randompersonName = "Jorge Berruecos";
+let age = 33;
+let isStudent = false;
+let address = undefined;
+let favouriteColor = null;
+const birthYear = 1991;
